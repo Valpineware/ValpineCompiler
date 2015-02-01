df8883900a4270614d2e3ebddabf9536fb118dbb
Dan Watkins <dwatkins@valpineware.com>
2015-02-01 13:09:29 -0600

    Cleanup

--- Src/Library/Parser/Recognizer.cpp
+++ Src/Library/Parser/Recognizer.cpp
@@ -9,6 +9,113 @@
 
 namespace vc { namespace parser
 {
+	graph::Function* parseType(QStringListIterator &i, graph::Function *function)
+	{
+		//TODO use the new QRegularExpression class instead
+		QRegExp typeMod("(const|&|\\*|\\s)*");
+		QRegExp typeId("(const|&|\\*|\\s|([A-Za-z_]\\w*))*");
+		QString returnType;
+		bool foundBaseType = false;
+			
+		while (i.hasNext())
+		{
+			const QString &cmp = i.next();
+
+			if (typeMod.exactMatch(cmp))
+				returnType.append(" "+cmp);
+			else if (!foundBaseType && typeId.exactMatch(cmp))
+			{
+				foundBaseType = true;
+				returnType.append(" "+cmp);
+			}
+			else if (returnType.isEmpty())
+				return nullptr;								//Error: No valid return type
+			else
+			{
+				i.previous();
+				function->setReturnType(returnType);
+				break;
+			}
+		}			
+
+		//	c) Identifier
+		if (i.hasNext())
+		{
+			const QString &cmp = i.next();
+
+			if (QRegExp("([A-Za-z_]\\w*)").exactMatch(cmp))
+			{
+				function->setId(cmp);
+			}
+			else
+				return nullptr;								//Error: invalid identifier
+		}
+		else
+			return nullptr;									//Error: nothing following return type
+
+		return function;
+	}
+
+
+
+	graph::Function* parseParameters(QStringListIterator &i, graph::Function *function)
+	{
+		while (i.hasNext())
+		{
+			QRegExp typeMod("(const|&|\\*|\\s)*");
+			QRegExp typeId("(const|&|\\*|\\s|([A-Za-z_]\\w*))*");
+			graph::Parameter prm;
+			bool foundBaseType = false;
+			bool foundEndTag = false;
+
+			//get the return type
+			while (i.hasNext())
+			{
+				const QString &cmp = i.next();
+
+				if (cmp.contains(")"))
+				{
+					foundEndTag = true;
+					break;
+				}
+				else if (typeMod.exactMatch(cmp))
+					prm.type.append(" "+cmp);
+				else if (!foundBaseType && typeId.exactMatch(cmp))
+				{
+					foundBaseType = true;
+					prm.type.append(" "+cmp);
+				}
+				else if (prm.type.isEmpty())
+					return nullptr;								//Error: No valid return type
+				else
+				{
+					i.previous();
+					break;
+				}
+			}
+
+			//get the argument name
+			if (i.hasNext())
+			{
+				const QString &cmp = i.next();
+
+				if (QRegExp("([A-Za-z_]\\w*)").exactMatch(cmp))
+				{
+					prm.id = cmp;
+					function->addParameter(prm);
+				}
+				else
+					return nullptr;								//Error: invalid identifier
+			}
+			else if (!foundEndTag)
+				return nullptr;									//Error: nothing following return type
+		}
+
+		return function;
+	}
+
+
+
 	graph::Function* Recognizer::parseFunctionSignature(const QString &signature)
 	{
 		//early exit tests
@@ -43,53 +150,9 @@ namespace vc { namespace parser
 		//4) Iterate through the list. Build the Function instance from here. If an error is detected, null the function instance and return
 		//	a) Pre-Modifiers TODO
 
-		//	b) Return type
-		{
-			//TODO use the new QRegularExpression class instead
-			QRegExp typeMod("(const|&|\\*|\\s)*");
-			QRegExp typeId("(const|&|\\*|\\s|([A-Za-z_]\\w*))*");
-			QString returnType;
-			bool foundBaseType = false;
-			
-			while (i.hasNext())
-			{
-				const QString &cmp = i.next();
-
-				if (typeMod.exactMatch(cmp))
-					returnType.append(" "+cmp);
-				else if (!foundBaseType && typeId.exactMatch(cmp))
-				{
-					foundBaseType = true;
-					returnType.append(" "+cmp);
-				}
-				else if (returnType.isEmpty())
-					return nullptr;								//Error: No valid return type
-				else
-				{
-					i.previous();
-					function->setReturnType(returnType);
-					break;
-				}
-			}			
-		}
-
-		//	c) Identifier
-		{
-			if (i.hasNext())
-			{
-				const QString &cmp = i.next();
-
-				if (QRegExp("([A-Za-z_]\\w*)").exactMatch(cmp))
-				{
-					function->setId(cmp);
-				}
-				else
-					return nullptr;								//Error: invalid identifier
-			}
-			else
-				return nullptr;									//Error: nothing following return type
-		}
-
+		//Return type
+		if (! (function = parseType(i, function)))
+			return nullptr;
 
 		//is it time for arguments yet?
 		if (i.hasNext())
@@ -98,59 +161,9 @@ namespace vc { namespace parser
 				return nullptr;
 		}
 
-		//	d) Arguments
-		{
-			while (i.hasNext())
-			{
-				QRegExp typeMod("(const|&|\\*|\\s)*");
-				QRegExp typeId("(const|&|\\*|\\s|([A-Za-z_]\\w*))*");
-				graph::Parameter prm;
-				bool foundBaseType = false;
-				bool foundEndTag = false;
-
-				//get the return type
-				while (i.hasNext())
-				{
-					const QString &cmp = i.next();
-
-					if (cmp.contains(")"))
-					{
-						foundEndTag = true;
-						break;
-					}
-					else if (typeMod.exactMatch(cmp))
-						prm.type.append(" "+cmp);
-					else if (!foundBaseType && typeId.exactMatch(cmp))
-					{
-						foundBaseType = true;
-						prm.type.append(" "+cmp);
-					}
-					else if (prm.type.isEmpty())
-						return nullptr;								//Error: No valid return type
-					else
-					{
-						i.previous();
-						break;
-					}
-				}
-
-				//get the argument name
-				if (i.hasNext())
-				{
-					const QString &cmp = i.next();
-
-					if (QRegExp("([A-Za-z_]\\w*)").exactMatch(cmp))
-					{
-						prm.id = cmp;
-						function->addParameter(prm);
-					}
-					else
-						return nullptr;								//Error: invalid identifier
-				}
-				else if (!foundEndTag)
-					return nullptr;									//Error: nothing following return type
-			}
-		}
+		//Parameters
+		if (! (function = parseParameters(i, function)))
+			return nullptr;
 
 		//	e) Post-Modifiers
 
